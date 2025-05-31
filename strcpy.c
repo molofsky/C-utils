7080e787d7cccb4c174cf52152b93df24186f728
Adrian Molofsky <molofsky@stanford.edu>
2025-05-31 11:58:06 -0700

    Create strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,12 @@
+/* An implementation of the C library function strcpy 
+*
+* @author Adrian Molofsky
+* @version 1/19/23
+*/
+
+void strcpy (char x[], char y[]) {
+  int i = 0;
+  
+  while ((x[i] = y[i]) != '\0') /* copy & test byte */
+  i += 1;
+}
