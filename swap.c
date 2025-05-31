64b1c140a44f4ad18b1299b7dbd22916cac7dc8d
Adrian Molofsky <molofsky@stanford.edu>
2025-05-31 12:02:15 -0700

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+/* A generic swap function that swaps two void * arguments
+* of n byte size. 
+*
+* @author Adrian Molofsky
+* @version 1/9/23
+*/
+
+Void swap(void *data1ptr, void *data2ptr, size_t nbytes) {
+	char temp[nbytes]; 
+	memcpy(temp, data1ptr, nbytes);
+	memcpy(data1ptr, data2ptr, nbytes);
+	memcpy(data2ptr, temp, nbytes)
+} // the nbytes will decide the size of the datatype in a function
