b854553b47b34e9f82b5c014f960b3ba47029f62
Adrian Molofsky <molofsky@stanford.edu>
2025-05-31 12:00:25 -0700

    Create create_str.c

---
+++ create_str.c
@@ -0,0 +1,28 @@
+/* Function to create a dynamically allocated string given input allocation size and characters
+*
+* @author Adrian Molofsky
+* @version 10/19/22
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_string(char ch, int num) {
+    char *new_str = malloc(sizeof(char*) * (num + 1));
+    for (int i = 0; i < num; i++) {
+        new_str[i] = ch;
+    }
+    new_str[num] = '\0';
+    return new_str;
+}
+
+main(int argc, char *argv[]) {
+    if (argc == 3) {
+    char *str = create_string(argv[1], argv[2]);
+    printf("%s\n",str); 
+    return 0;
+    } else {
+        perror("Function accepts two arguments");
+    }
+}
